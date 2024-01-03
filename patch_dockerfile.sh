c6e1ad3264d97796c8101cfcb335e420d8fe6a1a
hosxy <hosxy@live.com>
2024-01-03 20:07:55 +0800

    Update patch_dockerfile.sh

--- patch_dockerfile.sh
+++ patch_dockerfile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-sed -i 's#https://gmplib.org/download/gmp#https://ftp.gnu.org/gnu/gmp' Dockerfile.mingw
+
 sed -i 's#cd aria2#cd aria2 \&\& curl -L -o aria2-fast.patch https://raw.githubusercontent.com/hosxy/aria2-fast/master/aria2-fast.patch \&\& patch -Np1 < ./aria2-fast.patch#' Dockerfile.mingw
 
